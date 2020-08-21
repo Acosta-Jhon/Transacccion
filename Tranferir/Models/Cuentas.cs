7d39347892e8d216e92229be8ab2e15a050f6fbc
Acosta-Jhon <jhonerick2018@gmail.com>
2020-08-21 08:13:56 -0500

    Desing View

--- Tranferir/Models/Cuentas.cs
+++ Tranferir/Models/Cuentas.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Tranferir.Models
@@ -8,10 +9,15 @@ namespace Tranferir.Models
     {
 
         public int Id { get; set; }
+        [Required]
         public int Digitos { get; set; }
+        [Required]
         public string Cedula { get; set; }
+        [Required]
         public string Nombre { get; set; }
+        [Required]
         public string Apellido { get; set; }
+        [Required]
         public float Dinero { get; set; }
 
     }
